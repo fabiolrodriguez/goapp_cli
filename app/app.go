dd2bcee02fddd0e1d9f34fd98342c48af7f8d39d
fabiolrodriguez <fabio.rodriguez@gmail.com>
2022-01-15 14:18:28 -0300

    ip search implemented

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar retorna a app pronta
 func Gerar() *cli.App {
@@ -8,5 +14,35 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação cli"
 	app.Usage = "Buscao e IPS e nomes de Servidor"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPs",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIPs,
+		},
+	}
+
 	return app
+}
+
+func buscarIPs(c *cli.Context) {
+	host := c.String("host")
+
+	//pacote net
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
 }
